c568479b9602eb611a92299144491e1809a4857d
Kourtney Smith <klsmith5370@gmail.com>
2023-04-21 15:50:14 -0400

    Created repository and added logo and styling

---
+++ client/components/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import {connect} from 'react-redux'
+
+/**
+ * COMPONENT
+ */
+export const Home = props => {
+  const {username} = props
+
+  return (
+    <div>
+      <h3>Welcome, {username}</h3>
+    </div>
+  )
+}
+
+
+const mapState = state => {
+  return {
+    username: state.auth.username
+  }
+}
+
+export default connect(mapState)(Home)
