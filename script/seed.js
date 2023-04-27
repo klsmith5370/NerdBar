e0233d1125d89650127be6f7e00a269e4b99f1cc
Kourtney Smith <klsmith5370@gmail.com>
2023-04-27 19:22:41 -0400

    Seed file complete

--- script/seed.js
+++ script/seed.js
@@ -1,6 +1,6 @@
 'use strict'
 
-const {db, models: {User, CharacterRecipes} } = require('../server/db')
+const { db, models: { User, CharacterRecipe } } = require('../server/db/index')
 
 
 /**
@@ -17,52 +17,47 @@ async function seed() {
     User.create({ username: 'yuna', password: '123' }),
   ])
 
+  // Creating recipes
   const characterRecipes = await Promise.all([
-    CharacterRecipes.create({
+    CharacterRecipe.create({
       characterName: 'Nezuko',
       recipeName: 'Blood Demon Art',
       recipeDescription: '2 oz vodka, fill with cranberry or raspberry juice, a splash of soda and grenadine, add lychee jelly for garnish',
-      recipeCategory: 'Alcoholic, drinks',
       characterImage: 'https://cdn.epicstream.com/images/ncavvykf/epicstream/1291d35e00f077741819c23a19e5043c0a54e8fc-864x382.png'
     }),
 
-    CharacterRecipes.create({
+    CharacterRecipe.create({
       characterName: 'Anya Forger',
       recipeName: 'WAKU WAKU!',
       recipeDescription: 'fill glass with your favorite lemon-lime soda and mix with pink cranberry juice, garnish with cut-out lemon stars',
-      recipeCategory: 'Non-alcoholic, drinks',
       characterImage: 'https://img1.ak.crunchyroll.com/i/spire2/59a30d14cee549bafa1f0ba5f6f4e6681650356534_full.png'
     }),
 
-    CharacterRecipes.create({
-      characterName: '',
-      recipeName: '',
-      recipeDescription: '',
-      recipeCategory: 'Non-alcoholic, drinks',
-      characterImage: ''
+    CharacterRecipe.create({
+      characterName: 'Sailor Moon',
+      recipeName: 'Moon Prism Power',
+      recipeDescription: 'fill bottom of glass with grendadine, 1 shot of fireball whiskey and 1 shot of vanilla vodka, fill with white peach juice, top with blue curacao',
+      characterImage: 'https://screenrant.com/wp-content/uploads/2019/12/Sailor-Moon-Cropped.jpg'
     }),
 
-    CharacterRecipes.create({
+    CharacterRecipe.create({
       characterName: 'Morgana',
-      recipeName: 'Not A Cat!',
-      recipeDescription: '1 oz tequila of your choice, 1 oz rum of your choice, ice, blackberry/black cherry juice of your choice, garnish with black sea salt around the rim',
-      recipeCategory: 'Alcoholic, drinks',
+      recipeName: 'Not A Cat',
+      recipeDescription: '1 shot tequila of your choice, 1 shot rum of your choice, ice, blackberry/black cherry juice of your choice, garnish with black sea salt around the rim',
       characterImage: 'https://cdn.shopify.com/s/files/1/0101/1036/9888/products/UDONStore-Product-P5Shirt-MorganaInsta_1080x.jpg'
     }),
 
-    CharacterRecipes.create({
+    CharacterRecipe.create({
       characterName: 'Sephiroth',
       recipeName: 'One Winged Angel',
-      recipeDescription: '',
-      recipeCategory: 'Alcholic, drinks',
+      recipeDescription: '1 shot of whiskey of your choice, mix with Horchata, line glass with cinnamon',
       characterImage: 'https://rare-gallery.com/uploads/posts/332547-Sephiroth-Final-Fantasy-7-Remake-4K.jpg'
     }),
 
-    CharacterRecipes.create({
+    CharacterRecipe.create({
       characterName: 'Princess Peach',
       recipeName: 'Peach Kingdom',
-      recipeDescription: '2 oz peach schnapps, 2 oz lemon vodka, ',
-      recipeCategory: 'Alcoholic, drinks',
+      recipeDescription: 'Half pink lemonade, half peach juice, add edible glitter, mix, garnish with a peach slice and umbrella',
       characterImage: 'https://play.nintendo.com/images/profile-mk-peach.7bf2a8f2.aead314d58b63e27.png'
     }),
   ])
