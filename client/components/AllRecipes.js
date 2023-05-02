9f6e8630b56af53374b9d15fd182e11bf58cefc8
Kourtney Smith <klsmith5370@gmail.com>
2023-05-02 14:55:09 -0400

    Continued working on recipe card component

--- client/components/AllRecipes.js
+++ client/components/AllRecipes.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react'
 import { useSelector } from 'react-redux'
 
+
 export const AllRecipes = () => {
     let recipes = useSelector((state) => state.recipes)
+
 }
--- client/components/recipeCard.js
+++ client/components/recipeCard.js
@@ -1,9 +1,42 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import Card from '@mui/material/Card'
+import CardContent from '@mui/material/CardContent'
+import CardMedia from '@mui/material/CardMedia'
+import Typography from '@mui/material/Typography'
+import { CardActionArea } from '@mui/material'
+import Container from '@mui/material/Container'
 
 export const RecipeCard = (props) => {
-    const { recipe } = props
-    
+    const { recipes } = props
+
     return (
-        <div></div>
+        <Container fluid>
+            {recipes.map((recipe) => (
+                <Card key={recipe.id} sx={{ maxWidth: 345 }} className='recipe-card'>
+                    <CardActionArea>
+                        <Link to={`/characterRecipes/${recipe.id}`} style={{ color: 'inherit' }}>
+                            <CardMedia
+                                component='img'
+                                height='140'
+                                image={recipe.characterImage}
+                                alt='character image'
+                            />
+                        </Link>
+                        <CardContent>
+                            <Typography gutterBottom variant='h5' component='div'>
+                                {recipe.characterName}
+                            </Typography>
+                            <Typography gutterBottom variant='h4'>
+                                {recipe.recipeName}
+                            </Typography>
+                            <Typography variant='body2' color='text.secondary'>
+
+                            </Typography>
+                        </CardContent>
+                    </CardActionArea>
+                </Card>
+            ))}
+        </Container>
     )
 }
