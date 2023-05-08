f12257327d85b97928055797e8b9bdf18345760a
Kourtney Smith <klsmith5370@gmail.com>
2023-05-08 15:18:16 -0400

    Updated RecipeCard component to have a grid layout

--- client/components/AllRecipes.js
+++ client/components/AllRecipes.js
@@ -1,7 +1,6 @@
 import React, { useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import { Container } from '@material-ui/core'
-import { Grid } from '@material-ui/core'
 import { RecipeCard } from './recipeCard'
 import { fetchAllRecipes } from '../store/recipes'
 
@@ -14,42 +13,15 @@ export const AllRecipes = () => {
 
     return (
         <Container className='all-recipes'>
-            <Grid container spacing={2}>
-                <Grid item xs={14} sm={8} md={6}>
-                    <RecipeCard />
-                </Grid> 
-            </Grid>
+            <RecipeCard />
+
         </Container>
     )
 
-    // return (
-    //     <Container className='all-recipes'>
-    //         <Grid container spacing={2}>
-    //             {recipes.map((recipe) => (
-    //                 <Grid item xs={14} sm={8} md={6} key={recipe.id}>
-    //                     <RecipeCard />
-    //                 </Grid>
-    //             ))}
-    //         </Grid>
-    //         {/* <Pagination count={10} variant="outlined" color="secondary" /> */}
-    //     </Container>
-    // )
+    // Notes for future:
+        // for pagination: {/* <Pagination count={10} variant="outlined" color="secondary" /> */}
 
 }
 
 
 export default AllRecipes
-
-
-// if wanting to use regular React
-    // return (
-    //     <div className='all-recipes-container'>
-    //         <div>
-    //             {recipes.map((recipe) => (
-    //                 <div key={recipe.id}>
-    //                     <RecipeCard />
-    //                 </div>
-    //             ))}
-    //         </div>
-    //     </div>
-    // )
--- client/components/recipeCard.js
+++ client/components/recipeCard.js
@@ -1,58 +1,40 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import { Container, Card, CardContent, CardMedia, Typography, CardActionArea } from '@material-ui/core'
+import { Container, Grid, Card, CardContent, CardMedia, Typography, CardActionArea } from '@material-ui/core'
 
 export const RecipeCard = () => {
     const recipes = useSelector(state => state.recipes)
 
+
     return (
-        <Container className='recipe-card'>
-            {recipes.map((recipe) => (
-                <Card key={recipe.id} sx={{ maxWidth: 345 }}>
-                    <CardActionArea>
+        <Container>
+            <Grid container spacing={2}>
+                {recipes.map((recipe) => (
+                    <Grid item xs={12} sm={6} md={6} lg={6}>
+                        <Card key={recipe.id} sx={{ maxWidth: 345 }}>
+                            <CardActionArea>
+                                <Link to={`/characterRecipes/${recipe.id}`}>
+                                    <CardMedia 
+                                        component='img'
+                                        height='140'
+                                        image={recipe.characterImage}
+                                        alt='character image'
+                                    />
+                                </Link>
 
-                    <Link to={`/characterRecipes/${recipe.id}`}>
-                        <CardMedia
-                            component='img'
-                            height='140'
-                            image={recipe.characterImage}
-                            alt='character image'
-                        />
-                        </Link>
-
-                        <CardContent>
-                            <Typography gutterBottom variant='h4' component='div'>             
-                                    {recipe.characterName}
-                            </Typography>
-                        </CardContent>
-
-                    </CardActionArea>
-                </Card>
-        ))}
+                                <CardContent>
+                                    <Typography gutterBottom variant='h4' component='div'>
+                                        {recipe.characterName}
+                                    </Typography>
+                                </CardContent>
+                            </CardActionArea>
+                        </Card>
+                    </Grid>
+                ))}
+            </Grid>
         </Container>
     )
 }
 
 export default RecipeCard
-
-// if wanting to use regular React
-    // return (
-    //     <div className='recipe-card-container'>
-    //         {recipes.map(recipe => (
-    //             <div key={recipe.id} className='recipe-card'>
-    //                   <Link to={`/characterRecipes/${recipe.id}`} style={{ color: 'inherit' }}>
-    //                     <img src={recipe.characterImage} alt='character image' className='recipe-card-image' />
-    //                   </Link>
-
-    //                   <div>
-    //                     <h3>{recipe.characterName}</h3>
-    //                     <h2>{recipe.recipeName}</h2>
-    //                     <p>{recipe.recipeDescription}</p>
-    //                     <p>{recipe.recipeIngredients}</p>
-    //                     <p>{recipe.recipeInstructions}</p>
-    //                   </div>
-    //             </div>
-    //         ))}
-    //     </div>
-    // )
