f01c9169023a4beffb64a235de8475c3fde1f46f
Kourtney Smith <klsmith5370@gmail.com>
2023-05-11 17:47:00 -0400

    Working on updating navbar component

--- client/components/Navbar.js
+++ client/components/Navbar.js
@@ -2,26 +2,31 @@ import React from 'react'
 import {connect} from 'react-redux'
 import {Link} from 'react-router-dom'
 import {logout} from '../store'
+import { AppBar, Box, ToolBar, InputBase, SearchIcon } from '@material-ui/core'
 
 const Navbar = ({handleClick, isLoggedIn}) => (
   <div>
     <nav className="navbar">
       {isLoggedIn ? (
-        <div>
+        <Box sx={{ flexGrow: 1, flexDirection: 'row', display: 'flex', alignItems: 'center' }}>
+          <AppBar>
           {/* The navbar will show these links after you log in */}
           <Link to="/home">Home</Link>
           <a href="#" onClick={handleClick}>
             Logout
           </a>
-        </div>
+          </AppBar>
+        </Box> 
       ) : (
-        <div>
+        <Box sx={{ flexGrow: 1}}>
+          <AppBar position='static'>
           {/* The navbar will show these links before you log in */}
-          <img className='logo' src='NerdBar-logo.jpg' alt='NerdBar brand logo'/>
-          <Link to="/login">Login</Link>
-          <Link to="/signup">Sign Up</Link>
-          <Link to='/home'>Home</Link>
-        </div>
+            <img className='logo' src='NerdBar-logo.jpg' alt='NerdBar brand logo'/>
+            <Link to="/login">Login</Link>
+            <Link to="/signup">Sign Up</Link>
+            <Link to='/home'>Home</Link>
+          </AppBar>
+        </Box> 
         
       )}     
     </nav>
