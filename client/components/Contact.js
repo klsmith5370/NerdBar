a9d373beffa970e77a6c82b5c9abae65a10eafd6
Kourtney Smith <klsmith5370@gmail.com>
2023-05-26 15:26:50 -0400

    Added icons for TikTok & Instagram on contact page

--- client/components/Contact.js
+++ client/components/Contact.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
+import { SiTiktok } from 'react-icons/si'
+import { GrInstagram } from 'react-icons/gr'
 
 export const Contact = () => {
 
     return (
         <div>
             <h2>Follow us on Instagram & TikTok!</h2>
+            <Link></Link>
+            <Link><SiTiktok /></Link>
+            <Link><GrInstagram /></Link>
         </div>
     )
 }
