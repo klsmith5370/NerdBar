2531783ac7d74c406601ffc0d2832135cdebb05a
Kourtney Smith <klsmith5370@gmail.com>
2023-07-31 14:04:14 -0400

    Updated Contact component tobe a footer

--- client/App.js
+++ client/App.js
@@ -47,10 +47,11 @@ const App = () => {
             <Route path='/login' element={<Login />} />
             <Route path='/signup' element={<Signup />} />
             <Route path='/characterRecipes' element={<AllRecipes />} />
-            <Route path='/follow' element={<Contact />} />
+            {/* <Route path='/follow' element={<Contact />} /> */}
             <Route path='/search/:characterName' element={<Search />} />
           </Routes>
           )}
+          <Contact />
         </div>
   )
 }
--- client/components/Contact.js
+++ client/components/Contact.js
@@ -2,15 +2,43 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { SiTiktok } from 'react-icons/si'
 import { GrInstagram } from 'react-icons/gr'
+import { Container, Typography, Grid } from '@material-ui/core'
+import { styled } from '@material-ui/core'
+
+const FooterContainer = styled('footer')(({ theme }) => ({
+    backgroundColor: '',
+    padding: theme.spacing(6),
+    marginTop: 'auto',
+}))
+
+const CenteredGridItem = styled(Grid)(({ theme }) => ({
+    textAlign: 'center',
+}))
 
 export const Contact = () => {
 
     return (
-        <div className='follow'>
-            <h2>Follow us on Instagram & TikTok!</h2>
-            <Link className='icon-link'><SiTiktok /></Link>
-            <Link className='icon-link'><GrInstagram /></Link>
-        </div>
+        <FooterContainer>
+            <Container maxWidth='lg'>
+                <Grid container spacing={2} justifyContent='center'>
+                    <Grid item xs={12}>
+                        <Typography>
+                            Follow us on Instagram & TikTok!
+                        </Typography>
+                    </Grid>
+                <CenteredGridItem item xs={6} sm={3}>
+                    <Link className='icon-link'>
+                        <SiTiktok />
+                    </Link>
+                </CenteredGridItem>
+                <CenteredGridItem item xs={6} sm={3}>
+                    <Link className='icon-link'>
+                        <GrInstagram />
+                    </Link>
+                </CenteredGridItem>
+                </Grid>
+            </Container>
+        </FooterContainer>
     )
 }
 
