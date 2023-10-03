42265a79f6224009a41c0e7724e6d873e93d97a0
Kourtney Smith <klsmith5370@gmail.com>
2023-10-03 15:57:55 -0400

    Added additional check for checking if own profile

--- client/components/Banner.js
+++ client/components/Banner.js
@@ -5,10 +5,14 @@ import { Container } from '@material-ui/core'
 const Banner = (props) => {
     const { user } = props
     let auth = useSelector((state) => state.auth)
+    const isOwnProfile = () => {
+        return auth.id === user.id
+    }
+    const dispatch = useDispatch()
 
     return (
         <Container>
-            
+
         </Container>
     )
 
