1b513adafc7ea8aeaa7e6c51671524f7b9fbdef1
Kourtney Smith <klsmith5370@gmail.com>
2023-12-14 15:13:25 -0500

    Store for favorite recipe added

--- client/store/userRecipe.js
+++ client/store/userRecipe.js
@@ -2,13 +2,23 @@ import axios from 'axios'
 
 const GET_FAVORITE_RECIPE = "FAVORITE_RECIPE"
 
-const getFavoriteRecipe = (userRecipe) => {
+const getFavoriteRecipe = (favoriteBook) => {
     return {
         type: GET_FAVORITE_RECIPE,
-        userRecipe,
+        favoriteBook,
     }
 }
 
-export const fetchFavoriteRecipe = (userId) => {
-    const { data } = await axios.get(`/api/characterRecipes/favoriteRecipe`)
+export const fetchFavoriteRecipe = (userId) => async (dispatch) => {
+    const { data } = await axios.get(`/api/characterRecipes/favoriteRecipe/${userId}`)
+    dispatch(getFavoriteRecipe(data))
+}
+
+export default function (state = {}, action) {
+    switch (action.type) {
+        case GET_FAVORITE_RECIPE:
+            return action.favoriteBook
+        default:
+            return state
+    }
 }
