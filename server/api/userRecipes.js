04ab08478fcfde439d90079032bb7de00691d952
Kourtney Smith <klsmith5370@gmail.com>
2023-12-29 13:38:51 -0500

    Added route for creating new favorites, will need to be tested

--- server/api/userRecipes.js
+++ server/api/userRecipes.js
@@ -6,9 +6,40 @@ const {
 
 module.exports = router;
 
+router.post("/favoriteRecipe/:userId/:recipeId", async (req, res, next) => {
+    try {
+        const { userId, recipeId } = req.params;
+
+        // Check if the user_recipe record already exists
+        let userRecipe = await User_Recipe.findOne({
+            where: {
+                userId,
+                recipeId,
+            },
+        });
+
+        if (!userRecipe) {
+            // If it doesn't exist, create a new user_recipe record
+            userRecipe = await User_Recipe.create({
+                userId,
+                recipeId,
+                favorite: true,
+            });
+        } else {
+            // If it exists, update the favorite status
+            userRecipe.favorite = true;
+            await userRecipe.save();
+        }
+
+        res.json(userRecipe);
+    } catch (error) {
+        next(error);
+    }
+})
+
 router.get("/favoriteRecipe/:userId", async (req, res, next) => {
     try {
-        const recipes = await User_Recipe.findOne({
+        const recipes = await User_Recipe.findAll({
             where: {
                 userId: req.params.userId,
                 favorite: true,
@@ -19,4 +50,18 @@ router.get("/favoriteRecipe/:userId", async (req, res, next) => {
         next(error)
     }
 })
+
+// router.get("/favoriteRecipe/:userId", async (req, res, next) => {
+//     try {
+//         const recipes = await User_Recipe.findOne({
+//             where: {
+//                 userId: req.params.userId,
+//                 favorite: true,
+//             },
+//         })
+//         res.json(recipes)
+//     } catch (error) {
+//         next(error)
+//     }
+// })
 
