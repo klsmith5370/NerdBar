7cd9812d37a9da67ea02f2e9371eb9ab9d96ffc4
Kourtney Smith <klsmith5370@gmail.com>
2024-01-09 12:15:11 -0500

    Updates to user recipe redux store for favorites

--- client/store/userRecipe.js
+++ client/store/userRecipe.js
@@ -16,25 +16,12 @@ export const fetchUserRecipe = (userRecipe) => async (dispatch) => {
     dispatch(getUserRecipe(data))
 }
 
-// const GET_FAVORITE_RECIPE = "FAVORITE_RECIPE"
 
-// const getFavoriteRecipe = (favoriteRecipe) => {
-//     return {
-//         type: GET_FAVORITE_RECIPE,
-//         favoriteRecipe,
-//     }
-// }
-
-// export const fetchFavoriteRecipes = (userId) => async (dispatch) => {
-//     const { data } = await axios.get(`/api/userRecipes/favoriteRecipe/${userId}`)
-//     dispatch(getFavoriteRecipe(data))
-// }
-
-// export default function (state = {}, action) {
-//     switch (action.type) {
-//         case GET_FAVORITE_RECIPE:
-//             return action.favoriteRecipe
-//         default:
-//             return state
-//     }
-// }
+export default function (state = {}, action) {
+    switch (action.type) {
+        case GET_USER_RECIPE:
+            return action.userRecipe
+        default:
+            return state
+    }
+}
--- client/store/userRecipes.js
+++ client/store/userRecipes.js
@@ -1,22 +1,37 @@
 import axios from 'axios'
 
+const GET_FAVORITE_RECIPES = "GET_FAVORITE_RECIPES"
 const ADD_FAVORITE_RECIPE = 'ADD_FAVORITE_RECIPE'
 
-const addFavorites = (userRecipe) => {
+const addToFavorites = (userRecipe) => {
     return {
         type: ADD_FAVORITE_RECIPE,
         userRecipe,
+    } 
+}
+
+const getFavorites = (userRecipes) => {
+    return {
+        type: GET_FAVORITE_RECIPES,
+        userRecipes,
     }
 }
 
-export const fetchFavoriteRecipes = (userRecipes) => async (dispatch) => {
-    const { data } = await axios.post(`/api/userRecipes`, userRecipe)  
-     dispatch(addFavorites(data)) 
+export const addFavoriteRecipes = (userRecipes) => async (dispatch) => {
+    const { data } = await axios.post(`/api/userRecipes`, userRecipes)  
+     dispatch(addToFavorites(data)) 
+}
+
+export const fetchFavoriteRecipes = (userId) => async (dispatch) => {
+    const { data } = await axios.get(`/api/userRecipes/favoriteRecipe/${userId}`)
+    dispatch(getFavorites(data))
 }
 
 export default function (state = [], action) {
     switch (action.type) {
         case ADD_FAVORITE_RECIPE:
             return [...state, action.userRecipe]
+        case GET_FAVORITE_RECIPES:
+            return action.userRecipes
     }
 }
