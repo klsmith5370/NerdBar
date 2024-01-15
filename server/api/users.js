51822af703486d5aadf515fc7b5dbce093a809a1
Kourtney Smith <klsmith5370@gmail.com>
2024-01-15 12:48:42 -0500

    Included character recipe model in the API endpoints for users

--- server/api/users.js
+++ server/api/users.js
@@ -19,10 +19,27 @@ router.get('/', async (req, res, next) => {
 
 router.get('/:id', async(req, res, next) => {
   try {
-    const user = await User.findByPk(req.params.id)
+    const user = await User.findByPk(req.params.id, {
+      include: [CharacterRecipe],
+    })
     res.json(user)
 
   } catch (err) {
     next(err)
   }
 })
+
+router.put('/:id', async(req, res, next) => {
+  try {
+    let user = await User.findByPk(req.params.id)
+    await user.update(req.body)
+    user = await User.findByPk(req.params.id, {
+      include: [CharacterRecipe],
+    })
+    res.send(user)
+
+  } catch (err) {
+    next(err)
+  }
+})
+
